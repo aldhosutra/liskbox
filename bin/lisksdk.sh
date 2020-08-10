6258b9911427a78cf35d95db85b3f3b288f69a65
Aldo Suhartono Putra <aldhosutra@gmail.com>
2020-08-10 22:20:43 +0700

    fix link

--- bin/lisksdk.sh
+++ bin/lisksdk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Installing Node as $(whoami), Home: $HOME"
+echo "$1"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
