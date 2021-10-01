7c0f81f83b6d38058f97085b5c576728f782acca
Aldo Suhartono Putra <aldhosutra@gmail.com>
2021-10-01 13:05:49 +0700

    change node version

--- bin/lisksdk.sh
+++ bin/lisksdk.sh
@@ -7,6 +7,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install 12
+nvm install v12.22.3
 
-npm install --global --production lisk-commander
+npm install --global lisk-commander
